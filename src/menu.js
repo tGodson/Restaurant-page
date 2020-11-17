4e55f575513e6437ed48d460565158e645d57dd4
tGodson <tendongze95@gmail.com>
2020-11-17 19:25:57 +0100

    host app on github pages

--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,9 @@
 import './css/style.css';
+import Breakfast1 from './images/breakfast-1.jpg';
+import Breakfast2 from './images/breakfast-2.jpg';
+import Breakfast3 from './images/breakfast-3.jpg';
+import Breakfast4 from './images/breakfast-4.jpg';
+
 
 const menu = () => {
   const content = document.getElementById('content');
@@ -14,28 +19,28 @@ const menu = () => {
   const column1 = document.createElement('div');
   column1.setAttribute('class', 'column');
   const img1 = document.createElement('img');
-  img1.setAttribute('src', '../src/images/breakfast-1.jpg');
+  img1.setAttribute('src', `${Breakfast1}`);
   column1.appendChild(img1);
   gallery.appendChild(column1);
 
   const column2 = document.createElement('div');
   column2.setAttribute('class', 'column');
   const img2 = document.createElement('img');
-  img2.setAttribute('src', '../src/images/breakfast-2.jpg');
+  img2.setAttribute('src', `${Breakfast2}`);
   column2.appendChild(img2);
   gallery.appendChild(column2);
 
   const column3 = document.createElement('div');
   column3.setAttribute('class', 'column');
   const img3 = document.createElement('img');
-  img3.setAttribute('src', '../src/images/breakfast-3.jpg');
+  img3.setAttribute('src', `${Breakfast3}`);
   column3.appendChild(img3);
   gallery.appendChild(column3);
 
   const column4 = document.createElement('div');
   column4.setAttribute('class', 'column');
   const img4 = document.createElement('img');
-  img4.setAttribute('src', '../src/images/breakfast-4.jpg');
+  img4.setAttribute('src', `${Breakfast4}`);
   column4.appendChild(img4);
   gallery.appendChild(column4);
 };
